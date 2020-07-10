9f89aa2e4dcb258ca2ebb8be6d56a1750712996e
dchambers <dchambers@D-CHAMBERS-LL.fslads.fsl.org.jm>
2020-07-10 14:46:17 -0500

    I18N first commit. Hello world internationalized(English and french)

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.stacksimplify.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	//return string
 	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	@GetMapping("/helloworld")
@@ -21,4 +28,13 @@ public class HelloWorldController {
 		
 	}
 
+	@GetMapping("/hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(name= "Accept-Language", required=false) String locale) {
+		return messageSource.getMessage("label.hello",null, new Locale(locale));
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessagesInI18NFormat() {
+		return messageSource.getMessage("label.hello",null, LocaleContextHolder.getLocale());
+	}
 }
